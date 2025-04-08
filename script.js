fa72a9684505f7ebaec27da3e7483bb4bb4d71f5
Ankita R Wankhede <141350270+Ankita-WebDev@users.noreply.github.com>
2025-04-08 12:32:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function orderNow() {
+    alert("Thanks for choosing FoodieHub! Your order is on its way 🚀");
+  }
+  let submit = document.getElementById("submit");
+  submit.onclick = function(){
+    alert ("form submitted successfully");
+  };
